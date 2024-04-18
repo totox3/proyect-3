b569ff909b78c89483251d68f7eb0bc1a38158c9
totox3 <torreserickdaniel5@gmail.com>
2024-04-18 16:37:00 -0700

    changes in imageshow.jxs

--- src/componets/imageShow.jsx
+++ src/componets/imageShow.jsx
@@ -1,7 +1,9 @@
-function imageShow (){
+function imageShow ({ image }){
     return(
         <div>
-            <h1>  image Show</h1>
+            <img src={image.urls.small}
+            alt={image.alt-description} />
+           
         </div>
     )
 }
