7de79d841732012e89ea1be621e04827e92ef36e
totox3 <torreserickdaniel5@gmail.com>
2024-04-18 16:29:22 -0700

    changes in api.js and also in imagelist.jsx

--- src/api.js
+++ src/api.js
@@ -1,13 +1,14 @@
 import axios from  'axios '
 const SearchImage = async (term) => {
-    const url = ' '
+    const url = 'https://api.unplash.com/search/photos/?client_id=uJ8wJdXvQe1Zhh_uBQ_qdFkdasoQhNZNp9GOer4nRU4'
     const response = await axios.get(url, {
         params: {
-            query: ' cars'
+            query: term
 
         }
     })
     console.log(response)
+    return response.data.results
 
 }
 
--- src/componets/imagelist.jsx
+++ src/componets/imagelist.jsx
@@ -1,7 +1,15 @@
-function imageList (){
+import imageShow from ' ./ImageShow '
+
+function imageList ({images}){
+    const rederImage = images.map((image)  =>{
+        return <ImageShow key={image.id} image={image}/>
+
+    }) 
+
     return(
         <div>
-            <h1>  image List</h1>
+            <h1> {renderImage} </h1>
         </div>
     )
 }
+export default ImageList 
